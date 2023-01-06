7dd259204b80a73e63b17976eb531094856bb3cd
SSSVT-211 <michal.dvorak04@gmail.com>
2023-01-06 12:37:39 +0100

    6.1.2023 - Základy, dokončení

--- 01_Zaklady/script.js
+++ 01_Zaklady/script.js
@@ -26,3 +26,42 @@ function changeToYellow() {
 // let => novější, modernější, lepší
 // var => zastaralý, oldschool, horší
 
+// 6.1.2023 ---------------------------------------------------------------------------------------------
+
+// Vrací kolekci - pole, takže když s tím chceme pracovat, tak ho musíme projít.
+let pecka = document.querySelectorAll('p')
+
+/* Není to normální foreach, ale je to funke.
+ * - do parametru forEache jsem dal funkci, která se zavolá pro všechny položky toho listu
+ */
+
+pecka.forEach(function (odstavec){
+    // odstavec - jen pojmenování proměnné - jsou to ty jednotlivé odstavce
+    console.log(odstavec);
+    odstavec.addEventListener('click', changePara)
+    // Problém: potřebujeme předat parametr do funkce changePara, aby věděl, který odstavec má přebarvit.
+
+});
+
+function changePara(e) {
+
+    // Řešení: v this je odkaz na objekt, který funkci zavolal. V našem případě je to proměnná 'odstavec' - ten odstavec, na který jsme kliknuli.
+    console.log('kliknul jsem', this)
+
+    /* Řešení2: 'e' má v sobě informace o události, která se stala. - kliknutí, kam jsme klikli, na jaký element se kliklo,...
+     * takže v 'e.target' bude také uložené péčko, na který jsme klikli.
+     */
+    console.log(e);
+
+    this.classList.toggle('yellow')
+}
+
+
+// Mohl bych to zapsat i takhle - všechno do jedné funkce :
+
+// pecka.forEach(function (odstavec){
+//     odstavec.addEventListener('click', function (){
+//         this.classList.toggle('yellow')
+//     })
+// });
+
