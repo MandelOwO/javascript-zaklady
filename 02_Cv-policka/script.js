804c6e0a08f48164a9b027c41188f93c132fb445
SSSVT-211 <michal.dvorak04@gmail.com>
2023-01-06 12:56:56 +0100

    6.1.2023 - Cvičení - políčka

---
+++ 02_Cv-policka/script.js
@@ -0,0 +1,7 @@
+let policka = document.querySelectorAll('.item');
+policka.forEach(function (policko){
+    console.log(policko)
+    policko.addEventListener('click', function () {
+        this.classList.toggle('fill')
+    })
+})
