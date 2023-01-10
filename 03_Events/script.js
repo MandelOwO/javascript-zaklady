d93b493fda5baf0fb4e3dc4ca6f00f7cf7717dd8
SSSVT-06 <dvorakmichal@sssvt.cz>
2023-01-10 13:05:05 +0100

    10.1.2023 - Events

--- 03_Events/script.js
+++ 03_Events/script.js
@@ -1,13 +1,26 @@
-// Nestihli jsme dokončit
-
 
 let paras = document.querySelectorAll('p');
 let checkbox = document.querySelector('input[type=checkbox]')
 
-paras.forEach(function (p) {
-    p.addEventListener('click', changePara)
+checkbox.addEventListener('change', function (e) {
+    if (this.checked){
+        addListeners();
+    } else  {
+        removeListeners();
+    }
 })
 
+function addListeners() {
+    paras.forEach(function (p) {
+        p.addEventListener('click', changePara)
+    })
+}
+
+function removeListeners() {
+paras.forEach(function (p) {
+    p.removeEventListener('click', changePara)
+})
+}
 
 function changePara() {
     this.classList.toggle('yellow')
