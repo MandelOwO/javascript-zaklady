47774cd8889f9c9e8c9daa24f04254d5b61c7831
SSSVT-06 <dvorakmichal@sssvt.cz>
2023-01-11 12:29:02 +0100

    11.1.2023 - key down, key up

--- 06_key_events/script.js
+++ 06_key_events/script.js
@@ -7,7 +7,8 @@ let enterDisplay = document.querySelector('#enter')
 let escapeDisplay = document.querySelector('#escape')
 
 
-document.addEventListener('keydown', countKey)
+document.addEventListener('keydown', countKey);
+document.addEventListener('keyup', removeClass)
 
 function countKey(e) {
     console.log(e)
@@ -31,6 +32,30 @@ function countKey(e) {
             countEnter(1)
         }else{
             countEnter(-1)
+        }
+    }
+}
+
+function removeClass(e) {
+    if (e.key === " "){
+        if (!e.ctrlKey){
+            spaceDisplay.parentElement.classList.remove('add')
+        }else{
+            spaceDisplay.parentElement.classList.remove('remove')
+        }
+
+    }else if(e.key === "Escape"){
+        if (!e.ctrlKey){
+            escapeDisplay.parentElement.classList.remove('add')
+        }else{
+            escapeDisplay.parentElement.classList.remove('remove')
+        }
+
+    }else if (e.key === "Enter"){
+        if (!e.ctrlKey){
+            enterDisplay.parentElement.classList.remove('add')
+        }else{
+            enterDisplay.parentElement.classList.remove('remove')
         }
     }
 }
@@ -77,11 +102,11 @@ function countEnter(action) {
 }
 
 function blink(element, className) {
-    // console.log(element.parentElement)
-
     element.parentElement.classList.add(className)
 
-    setTimeout(function () {
-        element.parentElement.classList.remove(className)
-    }, 100)
+    // setTimeout(function () {
+    //     element.parentElement.classList.remove(className)
+    // }, 100)
 }
+
+
