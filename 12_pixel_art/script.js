04b2ec0212d41ce12d0aa137c71f4ab7c96d2e27
dvorakmichal <misamisa@misa.misa>
2023-01-25 12:25:04 +0100

    25.1.2023 - malování

--- 12_pixel_art/script.js
+++ 12_pixel_art/script.js
@@ -1,18 +1,49 @@
-let container = document.querySelector("#canvas");
+let canvas = document.querySelector("#canvas");
+let colorPicker = document.querySelector("#colorPicker")
+let cells = [];
+
+let mouseDown = false;
+let color = "#000";
+
+colorPicker.value = "#000"
+
 
 function makeRows(rows, cols) {
-    container.style.setProperty('--grid-rows', rows);
-    container.style.setProperty('--grid-cols', cols);
+    canvas.style.setProperty('--grid-rows', rows);
+    canvas.style.setProperty('--grid-cols', cols);
 
     for (let c = 0; c < (rows * cols); c++) {
         let cell = document.createElement("div");
-        container.appendChild(cell).className = "grid-item";
-        cell.addEventListener('click', cellClick)
+        canvas.appendChild(cell).className = "grid-item";
+        cell.addEventListener('mouseover', draw)
+        cells.push(cell);
     }
 }
 
 makeRows(50, 50);
 
-function cellClick() {
-    this.style.backgroundColor = 'black';
+function draw() {
+    if (!mouseDown)
+        return;
+
+    this.style.backgroundColor = color;
 }
+
+document.querySelector("#btnDelete").addEventListener('click', function () {
+    cells.forEach(function (cell) {
+        cell.style.backgroundColor = "#fff"
+    })
+})
+
+document.addEventListener('mousedown', function () {
+    mouseDown = true;
+})
+
+document.addEventListener('mouseup', function () {
+    mouseDown = false;
+})
+
+colorPicker.addEventListener('input', function (e) {
+    color = this.value;
+    console.log(color);
+})
