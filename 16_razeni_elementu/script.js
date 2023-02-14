234e0f37efa9fd4f89687ae5baac66bf8b974906
MandelOwO <87827886+MandelOwO@users.noreply.github.com>
2023-02-14 20:29:33 +0100

    razeni elementu - přesouvání

--- 16_razeni_elementu/script.js
+++ 16_razeni_elementu/script.js
@@ -1,11 +1,12 @@
 let input = document.querySelector("#input");
 let button = document.querySelector("#button");
 let list = document.querySelector("#list");
+let err = document.querySelector("#error");
 
 button.addEventListener('click', addItem);
 
-
 function addItem() {
+    err.innerText = '';
     let text = input.value.trim();
     if (!checkInput(text)) {
         return;
@@ -16,10 +17,23 @@ function addItem() {
     let del = document.createElement('a');
     del.innerText = 'X';
     del.href = "#";
-
     del.addEventListener('click', e => removeItem(item));
 
+    let up = document.createElement('a');
+    up.innerText = '↑';
+    up.href = "#";
+    up.addEventListener('click', e => moveUp(item));
+
+    let down = document.createElement('a');
+    down.innerText = '↓';
+    down.href = "#";
+    down.addEventListener('click', e => moveDown(item));
+
     item.appendChild(document.createTextNode(text + ' ['));
+    item.appendChild(up);
+    item.appendChild(document.createTextNode(' | '))
+    item.appendChild(down);
+    item.appendChild(document.createTextNode(' | '))
     item.appendChild(del);
     item.appendChild(document.createTextNode(']'));
 
@@ -28,6 +42,23 @@ function addItem() {
     input.value = "";
 }
 
+function moveUp(item) {
+    if (!item.previousElementSibling) {
+        err.innerText = 'To nejde';
+        return;
+    }
+    err.innerText = '';
+    item.parentNode.insertBefore(item, item.previousElementSibling);
+}
+
+function moveDown(item) {
+    if (!item.nextElementSibling) {
+        err.innerText = 'To nejde';
+        return;
+    }
+    err.innerText = '';
+    item.parentNode.insertBefore(item, item.nextElementSibling.nextElementSibling);
+}
 
 function removeItem(item) {
     console.log(item)
@@ -36,9 +67,9 @@ function removeItem(item) {
 
 function checkInput(text) {
     if (text === "") {
+        err.innerText = 'To nejde';
         return false;
     }
+    err.innerText = '';
     return true;
 }
-
-
